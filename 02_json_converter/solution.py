9679b1ab702c1ae782e6e1a4f369526191321726
YaKarp1k <maximka.karpov.s@gmail.com>
2026-03-15 14:07:31 +0300

    не запускаются проверки, постоянно пишет ошибку в 5 задании, хотя я его и не запускаю. В некоторых заданиях, например 3, неправильно работает тест (было выяснено еще на паре)

--- 02_json_converter/solution.py
+++ 02_json_converter/solution.py
@@ -1,3 +1,9 @@
-# Напиши решение здесь
 import os
 import json
+import csv
+
+with open('users.csv', encoding='utf-8') as f:
+    users = list(csv.DictReader(f))
+
+with open('users.json', 'w', encoding='utf-8') as f:
+    json.dump(users, f)
--- 04_log_analysis/solution.py
+++ 04_log_analysis/solution.py
@@ -1,3 +1,14 @@
 # Напиши решение здесь
 import os
 import json
+
+count = 0
+
+
+with open('server.log', encoding='utf-8') as f:
+    for line in f:
+        if '404' in line:
+            count += 1
+
+with open('stats.txt', 'w', encoding='utf-8') as f:
+    f.write(str(count))
